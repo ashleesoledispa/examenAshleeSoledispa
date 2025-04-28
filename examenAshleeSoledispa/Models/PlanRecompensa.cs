43a3eee3896c647b0a9daa538f2bcfe7435841be
Ashlee Soledispa PC <ashlee.soledispa@udla.edu.ec>
2025-04-28 07:42:25 -0500

    Se agrego la clase parra la base de datos y se declaro las clases de modelos restantes

--- examenAshleeSoledispa/Models/PlanRecompensa.cs
+++ examenAshleeSoledispa/Models/PlanRecompensa.cs
@@ -1,6 +1,25 @@
-﻿namespace examenAshleeSoledispa.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace examenAshleeSoledispa.Models
 {
     public class PlanRecompensa
     {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaInicio { get; set; }
+        [Required]
+        public int PuntosAcumulados { get; set; }
+        [Required]
+        public string TipoRecompensa
+        {
+            get
+            {
+                return PuntosAcumulados < 500 ? "SILVER" : "GOLD";
+            }
+        }
     }
 }
--- examenAshleeSoledispa/Models/Reserva.cs
+++ examenAshleeSoledispa/Models/Reserva.cs
@@ -1,6 +1,20 @@
-﻿namespace examenAshleeSoledispa.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace examenAshleeSoledispa.Models
 {
     public class Reserva
     {
+        public int Id { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaEntrada { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaSalida { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal ValorPagar { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
     }
 }
