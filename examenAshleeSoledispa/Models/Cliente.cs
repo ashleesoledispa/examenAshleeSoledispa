ed21f4149a973fdc1dcccd883db66d18447491f5
Ashlee Soledispa PC <ashlee.soledispa@udla.edu.ec>
2025-04-28 08:34:13 -0500

    Se corrigio que presentaba problemas en el CRUD por no ubicar las key

--- examenAshleeSoledispa/Models/Cliente.cs
+++ examenAshleeSoledispa/Models/Cliente.cs
@@ -4,22 +4,25 @@ namespace examenAshleeSoledispa.Models
 {
     public class Cliente
     {
-        public int Id { get; set; }
+        [Key]
+        public int ClienteId { get; set; }
+
         [Required]
         [StringLength(100)]
         public string Nombre { get; set; }
-        [Required]
+
         [Range(18, 100)]
         public int Edad { get; set; }
-        [Required]
-        [DataType(DataType.Currency)]
-        public decimal Saldo { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
         [Required]
         public bool EsVip { get; set; }
-        [Required]
+
         [DataType(DataType.Date)]
         public DateTime FechaRegistro { get; set; }
-        public ICollection<Reserva> Reservas { get; set; }
 
+        public ICollection<Reserva> Reservas { get; set; }
     }
 }
